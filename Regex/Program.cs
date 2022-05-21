45b6e94dafb6e5ce3151834c204368dfb13bcef7
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-05-21 10:35:43 +0530

    [Debashis] insert User registration

---
+++ Regex/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace RegularExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problem Using Regular Expression!");
+
+            while (true)
+            {
+                Console.WriteLine("Please choose the option :\n1)Validating FirstName\n2)Validating LastName\n3)Email ID\n4)Mobile Number\n5)Password of 8digits\n6)Password with atleast 1 capital letter\n7)Atleast 1number\n8)Atleast 1 special Character\n9)Different Email Formats");
+                int option = Convert.ToInt16(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter your First Name");
+                        string FName = Console.ReadLine();
+                        UserRegistration first = new UserRegistration();
+                        first.FirstName(FName);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter your Last name: ");
+                        string LName = Console.ReadLine();
+                        UserRegistration last = new UserRegistration();
+                        last.LastName(LName);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter your emailId: ");
+                        string Email = Console.ReadLine();
+                        UserRegistration id = new UserRegistration();
+                        id.Email_Id(Email);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter your mobile number");
+                        string Mobile = Console.ReadLine();
+                        UserRegistration mob = new UserRegistration();
+                        mob.Mobile_Number(Mobile);
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter password");
+                        string Password = Console.ReadLine();
+                        UserRegistration pass = new UserRegistration();
+                        pass.PasswordRule1(Password);
+                        break;
+                    case 6:
+                        Console.WriteLine("Enter password");
+                        string Password2 = Console.ReadLine();
+                        UserRegistration pass2 = new UserRegistration();
+                        pass2.PasswordRule2(Password2);
+                        break;
+                    case 7:
+                        Console.WriteLine("Enter password");
+                        string Password3 = Console.ReadLine();
+                        UserRegistration pass3 = new UserRegistration();
+                        pass3.PasswordRule3(Password3);
+                        break;
+                    case 8:
+                        Console.WriteLine("Enter password");
+                        string Password4 = Console.ReadLine();
+                        UserRegistration pass4 = new UserRegistration();
+                        pass4.PasswordRule4(Password4);
+                        break;
+                    case 9:
+                        Console.WriteLine("Enter email id: ");
+                        string Email2 = Console.ReadLine();
+                        UserRegistration mail = new UserRegistration();
+                        mail.EmailValidation(Email2);
+                        break;
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
+    
+
---
+++ Regex/UserRegistration.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    class UserRegistration
+    {
+        public bool FirstName(string FName)
+        {
+            bool status;
+            string firstname = "^[A-Z]{1}[a-z]{2,}$";
+            Regex regex = new Regex(firstname);
+
+            if (regex.IsMatch(FName))
+            {
+                status = true;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("First name ({0}) is valid", FName);
+                Console.ResetColor();
+
+            }
+            else
+            {
+                status = false;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Please Enter First Letter Capital \n");
+                Console.ResetColor();
+
+            }
+            return status;
+        }
+        public bool LastName(String LName)
+        {
+            bool status;
+            string lastname = "^[A-z]{1}[a-z]{2,}$";
+            Regex regex = new Regex(lastname);
+
+            if (regex.IsMatch(LName))
+            {
+                status = true;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Last name ({0}) is valid", LName);
+                Console.ResetColor();
+
+            }
+            else
+            {
+                status = false;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Please Enter First Letter Capital \n");
+                Console.ResetColor();
+
+            }
+            return status;
+        }
+        public bool Email_Id(string Email)
+        {
+            bool status;
+            string email = "^[A-Za-z](.[a-z])+@[A-Za-z]+.[a-z]{2,4}(.[a-z]{2,3})?$";
+            Regex regex = new Regex(email);
+
+            if (regex.IsMatch(Email))
+            {
+                status = true;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("{0} is valid emailId.", Email);
+                Console.ResetColor();
+
+            }
+            else
+            {
+                status = false;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("{0} is not valid emailId.", Email);
+                Console.ResetColor();
+
+            }
+            return status;
+        }
+        public bool Mobile_Number(String Mobile)
+        {
+            bool status;
+            string number = "^(\\d{2}\\s)+\\d{10}$";
+            Regex regex = new Regex(number);
+            if (regex.IsMatch(Mobile))
+            {
+                status = true;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("{0} is valid mobile number.", Mobile);
+                Console.ResetColor();
+            }
+            else
+            {
+                status = false;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("{0} is not valid mobile number.", Mobile);
+                Console.ResetColor();
+
+            }
+            return status;
+        }
+        public bool PasswordRule1(string Password)
+        {
+            bool status;
+            string password = "^[a-za-z0-9]{8,}$";
+            Regex regex = new Regex(password);
+
+            if (regex.IsMatch(Password))
+            {
+                status = true;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Entered value is valid password.");
+                Console.ResetColor();
+            }
+            else
+            {
+                status = false;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Entered value is not valid password.");
+                Console.ResetColor();
+            }
+            return status;
+        }
+        public bool PasswordRule2(string Password2)
+        {
+            bool status;
+            var password = "^(?=.*[A-Z])[0-9a-zA-Z]{8,}$";
+            Regex regex = new Regex(password);
+
+            if (regex.IsMatch(Password2))
+            {
+                status = true;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Password is Valid.");
+                Console.ResetColor();
+
+            }
+            else
+            {
+                status = false;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please Enter valid Password.");
+                Console.ResetColor();
+
+            }
+            return status;
+        }
+        public bool PasswordRule3(string Password3)
+        {
+            bool status;
+            string password = "^(?=.*[0-9A-Z])[0-9a-zA-Z]{8,}$";
+            Regex regex = new Regex(password);
+
+            if (regex.IsMatch(Password3))
+            {
+                status = true;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Password is Valid.");
+                Console.ResetColor();
+
+            }
+            else
+            {
+                status = false;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please Enter valid Password.");
+                Console.ResetColor();
+
+            }
+            return status;
+        }
+        public bool PasswordRule4(string Password4)
+        {
+            bool status;
+            var password = "^(?=.*[@#$%0-9A-Z])[@#$%0-9a-zA-Z]{8,}$";
+            Regex regex = new Regex(password);
+
+            if (regex.IsMatch(Password4))
+            {
+                status = true;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Password is Valid.");
+                Console.ResetColor();
+
+            }
+            else
+            {
+                status = false;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please Enter valid Password.");
+                Console.ResetColor();
+            }
+            return status;
+        }
+        public bool EmailValidation(string Email)
+        {
+            bool status;
+            string email = "[A-Za-z0-9](.[-+a-z0-9A-Z])+@[A-Za-z0-9]+.[a-z]{2,4}(.[a-z]{2,3})?$"; //"^[A-Za-z0-9](.[A-Za-z0-9])+@[A-Za-z]+.[a-z]{2,4}(.[a-z]{2,3})?$";
+            Regex regex = new Regex(email);
+
+            if (regex.IsMatch(Email))
+            {
+                status = true;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Email id is valid");
+                Console.ResetColor();
+            }
+            else
+            {
+                status = false;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid email id");
+                Console.ResetColor();
+            }
+            return status;
+        }
+    }
+}
