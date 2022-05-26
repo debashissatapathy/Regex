e33897053f06d79fa55cf3e9d9cdc86a2e2aca45
Debashis <erdebashis@outlook.com>
2022-05-26 21:36:36 +0530

    [Debashis] Insert UserRegistration

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -2,9 +2,9 @@
 
 namespace RegularExpression
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Problem Using Regular Expression!");
 
